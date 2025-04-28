621451ed70119f2df3e30ea33e8fc489139d8449
Gadu25 <alexanderudag@gmail.com>
2025-04-28 15:01:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="flex gap-4 p-4 w-full">
+        <RouterLink to="/">Dashboard</RouterLink>
+        <RouterLink to="/my-tasks">My Tasks</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView class="p-4"/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/MyTasks.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="tasks">
+    <h1>My tasks</h1>
+  </div>
+</template>
+
+<style>
+</style>
