acb191bda289dd0e11786703a51460ccfb06ef68
Gadu25 <alexanderudag@gmail.com>
2025-05-03 14:52:43 +0800

    added loading component

---
+++ src/components/loaders/Loading.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+<template>
+    <div class="flex items-center justify-center w-full h-full gap-2">
+        <Icon icon="mynaui:circle-notch-solid" class="animate-spinSlow" width="24" height="24" />
+        <p class="text-gray-600">loading..</p>
+    </div>
+</template>
+<style scoped>
+.animate-spinSlow {
+    animation: spinSlow 1s linear infinite;
+}
+@keyframes spinSlow {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
